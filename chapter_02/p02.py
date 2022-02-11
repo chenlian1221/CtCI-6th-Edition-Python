9df6353c3fbc98da3ba2ff37c9673fec85d59ff0
chenlian1221 <chenlian1221@gmail.com>
2022-02-11 14:11:13 +0800

    update ch2p02

--- chapter_02/p02.py
+++ chapter_02/p02.py
@@ -2,18 +2,42 @@ from linkedlist import LinkedList, LinkedListNode
 from collections import defaultdict
 import unittest, time
 
-def find_kth_from_end(ll: LinkedList, k: int) -> int:
+def find_kth_from_end(ll: LinkedList, k: int) -> LinkedList:
     '''
     1. 先去count整個linked list長度 = c
     2. for loop從頭去找到 c-k
     '''
-    return
+
+    # node = ll.head
+    # c = 0
+    # while node:
+    #     c += 1
+    #     node = node .next  
+    # if k != c:  
+    #     node = ll.head
+    #     for i in range(c-k):
+    #         node = node.next
+    # else:
+    #     return ll.tail
+    # return ll.head
+
+    curr = temp = ll.head
+    for _ in range(k):
+        if not temp:
+            return None
+        temp = temp.next
+
+    while temp:
+        curr = curr.next
+        temp = temp.next
+    return curr
+
 
 class test(unittest.TestCase):
 
     test_cases = [
         # list, k, expected
-        ((10, 20, 30, 40, 50), 1, 50),
+        ([10, 20, 30, 40, 50], 1, 50),
         ((10, 20, 30, 40, 50), 5, 10),
     ]
 
@@ -26,10 +50,12 @@ class test(unittest.TestCase):
         runs = 1000
         fn_runtimes = defaultdict(float)
         for _ in range(runs):
-            for test, expected in self.test_cases:
+            for ll, n, expected in self.test_cases:
                 for fn in self.test_fns:
                     start = time.perf_counter()
-                    assert(fn(LinkedList(test)) == expected), f"{fn.__name__} failed for value {test}"
+                    f = fn(LinkedList(ll),n)
+                    # print(f.value,expected)
+                    assert(f.value == expected), f"{fn.__name__} failed for value {ll}, {n}"
                     fn_runtimes[fn.__name__] += (
                         time.perf_counter() - start
                     ) * 1000
